c0742a415bebddb5b06ae8740d0dbe614cfe4566
okosaleo <okosaleonel@gmail.com>
2024-12-24 23:15:52 +0100

    final changes

--- app/AuthProvider.tsx
+++ app/AuthProvider.tsx
@@ -1,10 +1,6 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
 
-export const AuthProvider = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+export const AuthProvider = ({children}) => {
   return <KindeProvider>{children}</KindeProvider>;
 };
--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,9 +1,11 @@
 import { Card } from "@/components/ui/card";
 import { SellForm } from "../components/form/SellForm";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 
 export default async function SellRoute() {
+  noStore();
   const {getUser} = getKindeServerSession()
   const user = await getUser()
 
